f731e54fd22dcb86870fd87eb962a858d33ce58b
vishaljoshy18 <1812-vishal@protonmail.com>
2022-08-20 05:41:29 +0530

    Add template metaprogramming.

---
+++ template-meta-programming/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+template <int n>
+struct adder
+{
+	enum
+	{
+		value = n + adder<n - 1>::value
+	};
+};
+
+template <>
+struct adder<0>
+{
+	enum
+	{
+		value = 0
+	};
+};
+
+int main(void)
+{
+	// std::cout << adder<10>::value;
+	int a = 10;
+	// int *ptr;
+	void * ptr = &a;
+	std::cout << *(int*)ptr;
+}
